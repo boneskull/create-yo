c72c7cf567b29475494fcaf858974077810ede5a
Christopher Hiller <boneskull@boneskull.com>
2018-11-13 11:59:26 -0800

    formatting

--- test/create-yo.spec.js
+++ test/create-yo.spec.js
@@ -15,16 +15,14 @@ describe('create-yo', function() {
       spy = sandbox.spy();
       spy.parseArgs = parseArgs;
       rewiremock.enable();
-      create =
-          rewiremock
-              .proxy(
-                  '..', r => ({
-                          which: r.with({
-                                    sync: sandbox.stub().returns('/path/to/npm')
-                                  }).directChildOnly(),
-                          libnpx: r.by(() => spy)
-                        }))
-              .create;
+      create = rewiremock.proxy('..', r => ({
+        which: r
+          .with({
+            sync: sandbox.stub().returns('/path/to/npm')
+          })
+          .directChildOnly(),
+        libnpx: r.by(() => spy)
+      })).create;
     });
 
     afterEach(function() {
@@ -34,23 +32,29 @@ describe('create-yo', function() {
 
     describe('when no generator specified', function() {
       it('should fail', function() {
-        expect(() => {
-          create(['/path/to/node', '/path/to/create-yo']);
-        }, 'to throw', /specify a generator to run/);
+        expect(
+          () => {
+            create(['/path/to/node', '/path/to/create-yo']);
+          },
+          'to throw',
+          /specify a generator to run/
+        );
       });
-    })
+    });
 
     describe('when no path to npm found', function() {
       it('should use `which` to find one', function() {
         create(['/path/to/node', '/path/to/create-yo', 'some-generator'], '');
         expect(spy, 'to have a call satisfying', {
-          args: [{
-            package: ['generator-some-generator@latest'],
-            cmdOpts: ['--', 'some-generator'],
-            npm: '/path/to/npm'
-          }]
+          args: [
+            {
+              package: ['generator-some-generator@latest'],
+              cmdOpts: ['--', 'some-generator'],
+              npm: '/path/to/npm'
+            }
+          ]
         });
       });
-    })
+    });
   });
 });
